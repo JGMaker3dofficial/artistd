91c8811c3b9fd4f95225a7724a9ef372678e358c
Michael Gardner <mikejr83@gmail.com>
2021-02-02 13:51:47 -0500

    Keep newly active extruder parked on tool change
    Enable `TOOLCHANGE_NO_RETURN` so that a newly activated extruder on a
    tool change g-code command remains parked when in IDEX Auto-Park mode.
    This overrides default Marlin configuration.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1704,7 +1704,7 @@
 #if EXTRUDERS > 1
   // Z raise distance for tool-change, as needed for some extruders
   #define TOOLCHANGE_ZRAISE     2  // (mm)
-  //#define TOOLCHANGE_NO_RETURN   // Never return to the previous position on tool-change
+  #define TOOLCHANGE_NO_RETURN     // Never return to the previous position on tool-change
 
   // Retract and prime filament on tool-change
   //#define TOOLCHANGE_FILAMENT_SWAP
